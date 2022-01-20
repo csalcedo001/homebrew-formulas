c8f6cf58d6b9bb4ae6dc5c5cb02bba857d224226
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2022-01-20 08:58:32 -0500

    Added formula for device

---
+++ device.rb
@@ -0,0 +1,13 @@
+
+class Device < Formula
+  desc "Device connection manager"
+  homepage ""
+  url "https://github.com/csalcedo001/device/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "c5e256c24a007bd096b04c38a027dfcba7063eb5580b94b604a2b0dae258a493"
+  license ""
+
+  def install
+    bin.install "device"
+  end
+
+end
