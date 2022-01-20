ca0ce876c1183cd1d382df127136efff35edd425
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2022-01-20 10:16:18 -0500

    Updated to v1.0.2

--- device.rb
+++ device.rb
@@ -2,8 +2,8 @@
 class Device < Formula
   desc "Device connection manager"
   homepage ""
-  url "https://github.com/csalcedo001/device/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "284bdbbaa5c2644e3707d39681c93bfe8277a3f4cd9025190da3a03b9790432c"
+  url "https://github.com/csalcedo001/device/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "ec915ba4f2f4ee665e51433fcfb3b0b5845d12ec808f4a5d844b19d41f0fc0a9"
   license ""
 
   def install
